1bf671f5d59e8a237d0f291640d02e2301f3982c
MuemaXe <muemarevans.com>
2025-01-15 09:38:57 +0300

    Custom Functions

--- mlp.go
+++ mlp.go
@@ -68,4 +68,75 @@ func(net Network) Predict(inputData[]float64)mat.Matrix{
 	return finalOutputs
 
 }
-func
+func sigmoid (r,c int ,z float64)float64{
+	return 1.0/(1+math.Exp(-1*z))
+}
+func sigmoidPrime(m mat.Matrix)m.Matrix{
+	rows,_ := m.Dims()
+	o := make([]float64,rows)
+	for i :=range o {
+		o[i]=1
+	}
+	ones:= mat.NewDense(rows,1,o)
+	return multiply (m,subtract(ones,m))
+}
+/*For example, the Gonum Product function allows us to perform the dot product operation on
+ two matrices, and I created a helper function that finds out the size of the matrix, 
+ creates it and perform the operation before returning the resultant matrix*/
+
+
+ /*The Dim function in the Go programming language is used to
+  find the maximum between the difference of two provided arguments and 0.
+  */
+func dot (m,n mat.Matrix)mat.Matrix{
+	r,_ := m.Dims()
+	_,c := n.Dims()
+	o :=mat.NewDense(r,c,nil)
+	o.Product(m,n)
+	return o
+}
+//the apply function allows us to apply a function to the matrix
+func apply (fn func(i,j int,v float64) float64,m mat.Matrix)mat.Matrix{
+	r,c := m.Dims()
+	o := mat.NewDense(r,c,nil)
+	o.Apply(fn,m)
+	return o
+}
+// the scale function allows us to use a matrix i.e multiply a matrix by a scalar
+func scale (s float64,m mat.Matrix)mat.Matrix{
+	r,c := m.Dims()
+	o := mat.NewDense(r,c,nil)
+	o.Scale(s,m)
+	return o
+}
+// The multiply function multiplies two functions together--This is differnt from dot Product
+func multiply(m,n mat.Matrix)mat.Matrix{
+	r,c := m.Dims()
+	o := mat.NewDense(r,c,nil)
+	o.MulElem(m,n)
+	return o
+}
+// The add and subtract functions allow to add or subtract a function to/from another
+func add(m,n mat.Matrix)mat.Matrix{
+	r,c := m.Dims()
+	o := mat.Dense(r,c,nil)
+	o.Add(m,n)
+	return o
+}
+func subtract(m,n mat.Matrix)mat.Matrix{
+	r,c := m.Dims()
+	o := mat.NewDense(r,c,nil)
+	o.Sub(m,n)
+	return o
+}
+// The addScalar function allows us to add a scalar value to each element in the matrix
+
+func addScalar (i float64,m mat.Matrix)mat.Matrix{
+	r,c := m.Dims()
+	a := make([]float64,r*c)
+	for x := 0; x<r*c;x++{
+		a[x]=i
+	}
+	n:= mat.NewDense(r,c,a)
+	return add(m,n)
+}
